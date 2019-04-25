c505080492ccc4f578fae977a81446b3d44887d6
Dyaksa Hanindito <dyaksadito@gmail.com>
2019-04-25 13:53:04 +0700

    quick reply test

--- app.py
+++ app.py
@@ -231,6 +231,34 @@ def handle_message(event):
             event.reply_token,
             message
         )
+    elif text == 'qr':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(
+                text='Quick reply',
+                quick_reply=QuickReply(
+                    items=[
+                        QuickReplyButton(
+                            action=PostbackAction(label="label1", data="data1")
+                        ),
+                        QuickReplyButton(
+                            action=MessageAction(label="label2", text="text2")
+                        ),
+                        QuickReplyButton(
+                            action=DatetimePickerAction(label="label3",
+                                                        data="data3",
+                                                        mode="date")
+                        ),
+                        QuickReplyButton(
+                            action=CameraAction(label="label4")
+                        ),
+                        QuickReplyButton(
+                            action=CameraRollAction(label="label5")
+                        ),
+                        QuickReplyButton(
+                            action=LocationAction(label="label6")
+                        ),
+                    ])))
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
