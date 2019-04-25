77fa28edfb03b31b7d5a2c0fd35a8f2f1474a767
Dyaksa Hanindito <dyaksadito@gmail.com>
2019-04-25 13:35:52 +0700

    button test

--- app.py
+++ app.py
@@ -85,7 +85,18 @@ def handle_message(event):
             MessageAction(label='No', text='No!'),
         ])
         template_message = TemplateSendMessage(
-            alt_text='Confirm alt text', template=ConfirmTemplate)
+            alt_text='Confirm alt text', template=confirm_template)
+        line_bot_api.reply_message(event.reply_token, template_message)
+    elif text == 'buttons':
+        buttons_template = ButtonsTemplate(
+            title='My buttons sample', text='Hello, my buttons', actions=[
+                URIAction(label='Go to line.me', uri='https://line.me'),
+                PostbackAction(label='ping', data='ping'),
+                PostbackAction(label='ping with text', data='ping', text='ping'),
+                MessageAction(label='Translate Rice', text='米')
+            ])
+        template_message = TemplateSendMessage(
+            alt_text='Buttons alt text', template=buttons_template)
         line_bot_api.reply_message(event.reply_token, template_message)
 
 
